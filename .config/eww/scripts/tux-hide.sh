64c4b87612705d9f1c87aecdd156a5102d0006a9
Mognus <magnusderkleine@gmail.com>
2026-06-05 19:11:32 +0200

    feat: add tux devil widget

--- .config/eww/scripts/tux-hide.sh
+++ .config/eww/scripts/tux-hide.sh
@@ -4,8 +4,8 @@ eww close tux-angel 2>/dev/null
 # poll cursor position until it leaves the bottom-right corner
 while true; do
     read x y <<< $(hyprctl cursorpos | tr ',' ' ')
-    # tux occupies roughly the last 100px on right, 136px from bottom (1080-136=944)
-    if [ "$x" -lt 1820 ] || [ "$y" -lt 944 ]; then
+    # tux occupies roughly the last 126px on right, 136px from bottom (1080-136=944)
+    if [ "$x" -lt 1794 ] || [ "$y" -lt 944 ]; then
         break
     fi
     sleep 0.1
