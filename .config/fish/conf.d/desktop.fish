f7af855b0b4047ec1a28b02168d6dd9ec2e4df24
Mognus <magnusderkleine@gmail.com>
2026-08-15 16:01:55 +0200

    fix: use systemd SSH agent socket in Fish

--- .config/fish/conf.d/desktop.fish
+++ .config/fish/conf.d/desktop.fish
@@ -3,6 +3,11 @@ if not status is-interactive; or not set -q WAYLAND_DISPLAY
     return
 end
 
+# Use the systemd-managed per-user SSH agent socket for this shell session.
+if set -q XDG_RUNTIME_DIR
+    set -gx SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+end
+
 function sc
     grim -g (slurp) ~/Pictures/screenshots/(date +%F_%T).png
 end
