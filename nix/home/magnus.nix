78542092b993ad61c18fc5fa84cf4ced550096e3
Mognus <magnusderkleine@gmail.com>
2026-09-26 18:45:07 +0200

    feat(nix): link nvim, tmux and git and install their tools

--- nix/home/magnus.nix
+++ nix/home/magnus.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 let
   dotfiles = "${config.home.homeDirectory}/dotfiles";
   # Link straight into the repo, like Stow did, so edits apply without a rebuild.
@@ -7,5 +7,20 @@ in
 {
   home.stateVersion = "26.05";
 
+  home.packages = with pkgs; [
+    # fish: frg, fco, fkill and the fzf key bindings
+    fzf
+    ripgrep
+    fd
+    bat
+    # nvim: nvim-treesitter compiles its parsers locally
+    tree-sitter
+    gcc
+    tmux
+  ];
+
   xdg.configFile."fish".source = link ".config/fish";
+  xdg.configFile."nvim".source = link ".config/nvim";
+  xdg.configFile."tmux".source = link ".config/tmux";
+  home.file.".gitconfig".source = link ".gitconfig";
 }
